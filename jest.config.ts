e7812feb6ac0b75e1bff28201ed07aa5f75354c8
hyeopsang <dbtkdguq3@gmail.com>
2026-02-27 05:25:49 +0900

    fix: Jest에서 e2e 테스트 제외

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,20 @@
-import type { Config } from 'jest'
+import nextJest from 'next/jest'
 
-const config: Config = {
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/e2e/',
+  ],
 }
 
-export default config
+export default createJestConfig(customJestConfig)
